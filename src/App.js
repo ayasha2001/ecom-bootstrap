eb0684edb44eab9b6796fe1381b781870f4eba50
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-01-07 21:55:42 +0530

    error fixing

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import { useState } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import StorePage from "./pages/StorePage";
+import { RouterProvider, createBrowserRouter,Outlet } from "react-router-dom";
 import NavigationComp from "./components/NavigationComp";
 import Cart from "./components/cart/Cart";
 import CartContextProvider from "./context/CartContextProvider";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ReactDOM from "react-dom/client";
-import { Outlet } from "react-router-dom";
-import FooterComp from "./components/FooterComp";
+import FooterComp from "./components/FooterComp"
+
 
 // const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
 
 function App() {
-  // const [cartOpen, setCartOpen] = useState(false);
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/store",
-      element: <StorePage />,
-    },
-    {
-      path: "/about",
-      element: <AboutPage />,
-    },
-  ]);
+  const [cartOpen, setCartOpen] = useState(false);
+  
   return (
     <CartContextProvider>
       {cartOpen && <Cart />}
--- src/components/ContainerComp.js
+++ src/components/ContainerComp.js
@@ -2,7 +2,35 @@ import React, { useState } from "react";
 import NavigationComp from "../components/NavigationComp";
 import Cart from "./cart/Cart";
 import { Container } from "react-bootstrap";
-import HeadingComp from "./HeadingComp";
+import {Row,Col} from "react-bootstrap"
+import ProductItem from "./ProductItem";
+
+export const productsArr = [
+  {
+    title: "Colors",
+    price: 100,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    id: Math.random(),
+  },
+  {
+    title: "Black and white Colors",
+    price: 50,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    id: Math.random(),
+  },
+  {
+    title: "Yellow and Black Colors",
+    price: 70,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    id: Math.random(),
+  },
+  {
+    title: "Blue Color",
+    price: 100,
+    imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    id: Math.random(),
+  },
+];
 
 const ContainerComp = ({ element }) => {
   const [cartOpen, setCartOpen] = useState(false);
@@ -12,7 +40,7 @@ const ContainerComp = ({ element }) => {
         <Row>
           {productsArr.map((product, index) => (
             <Col key={index}>
-              <Product item={product} />
+              <ProductItem item={product} />
             </Col>
           ))}
         </Row>
